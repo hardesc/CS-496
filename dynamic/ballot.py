f33daa917d270d761aaa5b34ccde1539b8145b07
Charles Hardes <charleshardes@gmail.com>
2016-10-09 17:13:29 -0700

    fixed list issue, starting to implement autofill of edit page

--- dynamic/ballot.py
+++ dynamic/ballot.py
@@ -2,6 +2,7 @@ import webapp2
 import base_page2
 from google.appengine.ext import ndb
 import db_defs
+import logging
 
 class Ballot(base_page2.BaseHandler):
     def __init__(self, request, response):
@@ -11,7 +12,7 @@ class Ballot(base_page2.BaseHandler):
     #refefines render from base_page2.BaseHandler to include template variables when data has been entered.
     #gets each database entry and stores them as template variables, calls render with template variables
     def render(self, page):
-        vq = db_defs.Vote.query().fetch()[-1]
+        vq = db_defs.Vote.query().fetch()[0]
         self.template_values['id'] = {'number':vq.VID, 'key':vq.key.urlsafe()}
         self.template_values['candidate'] = {'name':vq.candidate, 'key':vq.key.urlsafe()}
         self.template_values['issues'] = {'name':vq.issues, 'key':vq.key.urlsafe()}
@@ -20,9 +21,18 @@ class Ballot(base_page2.BaseHandler):
 
         base_page2.BaseHandler.render(self, page, self.template_values)
 
-    #don't actually know why this is necessary, if it is at all
     def get(self):
         self.template_values['cast'] = False
+        self.template_values['editing'] = True
+
+        if self.request.get('edit') == 'true':
+            url_key = self.request.get('key')
+            edit_key = ndb.Key(urlsafe=url_key)
+            edit_vote = edit_key.get()
+
+            #get all members of the vote to pass back into ballot.html via template_variables
+            self.template_values['vote_to_edit'] = {'VID':edit_vote.VID, 'candidate':edit_vote.candidate, 'issues':edit_vote.issues, 'email':edit_vote.email, 'phone':edit_vote.phone, 'key':edit_vote.key.urlsafe()}
+
         base_page2.BaseHandler.render(self, 'ballot.html', self.template_values)
 
     def post(self):
@@ -30,7 +40,9 @@ class Ballot(base_page2.BaseHandler):
         Vote = db_defs.Vote(parent=key)
         Vote.VID = int(self.request.get('id'))
         Vote.candidate = self.request.get('vote')
-        Vote.issues = self.request.get('issues', allow_multiple=True)     
+        
+
+        Vote.issues = self.request.get('issues', allow_multiple=True)
         Vote.email = self.request.get('email')
         Vote.phone = int(self.request.get('phone'))
         Vote.put()
--- dynamic/votes.py
+++ dynamic/votes.py
@@ -12,28 +12,9 @@ class Votes(base_page2.BaseHandler):
     #refefines render from base_page2.BaseHandler to include template variables when data has been entered.
     #gets each database entry and stores them as template variables, calls render with template variables
     def render(self, page):
-        """vote_list = []
-        i = 0
-        for vq in db_defs.Vote.query().fetch():
-
-            vote = votes(vq.VID, vq.candidate, vq.issues, vq.email, vq.phone, vq.key.urlsafe())
-            self.template_values['votes'] = vote_list.append(vote)
-"""
 
         self.template_values['votes'] = [{'VID':vq.VID, 'candidate':vq.candidate, 'issues':vq.issues, 'email':vq.email, 'phone':vq.phone, 'key':vq.key.urlsafe()} for vq in db_defs.Vote.query().fetch()]
 
-            #self.template_values['id'] = {'number':vq.VID, 'key':vq.key.urlsafe()}
-            #self.template_values['Vote' + str(i)] = vote
-            #i += 1
-
-        #self.template_values['key'] = [vq.key.urlsafe()]
-        """
-        self.template_values['id'] = [{'number':vq.VID, 'key':vq.key.urlsafe()} for vq in db_defs.Vote.query().fetch()]
-        self.template_values['candidate'] = [{'name':vq.candidate, 'key':vq.key.urlsafe()} for vq in db_defs.Vote.query().fetch()]
-        self.template_values['issues'] = [{'name':vq.issues, 'key':vq.key.urlsafe()} for vq in db_defs.Vote.query().fetch()]
-        self.template_values['email'] = [{'address':vq.email, 'key':vq.key.urlsafe()} for vq in db_defs.Vote.query().fetch()]
-        self.template_values['phone'] = [{'number':vq.phone, 'key':vq.key.urlsafe()} for vq in db_defs.Vote.query().fetch()]
-        """
         base_page2.BaseHandler.render(self, page, self.template_values)
         
     #don't actually know why this is necessary, if it is at all
