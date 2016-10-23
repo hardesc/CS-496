797e69bb6177ffdd53faff80b1d05f303524ca0e
Charles Hardes <charleshardes@gmail.com>
2016-10-23 10:04:38 -0700

    populating of dist_key_lists now implemented

--- Assignment3/admin.py
+++ Assignment3/admin.py
@@ -12,17 +12,21 @@ class Admin(base_page.BaseHandler):
 		self.initialize(request, response) #forgot why this is here
 
 	def post(self):
-		self.response.write("this is a post\n\n")
 
+		#===============================FULL DATABASE LOAD FROM SCRATCH==========================
 		if self.request.get('fill_all') == 'True':
 
+			self.nuke()#clear the entire db, since we're filling it from scratch
+
+			#===============GENERATE AND GET ALL OUR LISTS------------------------
 			L = Lists()
 			container_class_defs.generate_all_states(L)
 			container_class_defs.generate_all_districts(L)
 			container_class_defs.generate_all_electoral_college(L)
 			db_State_list = []
 			key_list = []
-
+									#==============STATES===============================
+			#===================GET THE STATES FROM THE CONTAINERS AND PUT THEM IN THE DB============
 			for i, our_state in enumerate(L.all_state_classes):
 
 				key_list.append(ndb.Key(db_defs.State, 'States'))
@@ -32,58 +36,72 @@ class Admin(base_page.BaseHandler):
 				db_State_list[i].quant_districts = our_state.quant_districts
 				db_State_list[i].quant_registered = our_state.quant_registered
 				db_State_list[i].quant_electors = our_state.quant_electors
-				db_State_list[i].dist_key_list = []
-				db_State_list[i].elector_key_list = []
+				#db_State_list[i].dist_key_list = []
+				#db_State_list[i].elector_key_list = []
 
-				#db_State_list.append(db_State)
+			state_keys = ndb.put_multi(db_State_list)#batch put all state keys in the db
 
-			#db_State.put()
-			state_keys = ndb.put_multi(db_State_list)
-
-			key_dict = {key.get().abbr: key for key in state_keys}
-
-
+			state_key_dict = {key.get().abbr: key for key in state_keys}#build a dict out of the state_keys list to use in District obj building
+									#==============================DISTRICTS======================
+			#============GET THE DISTRICTS FROM CONTAINERS, GET THEIR STATES, FILL STATE.DIST LISTS, PUT IN DB====
 			db_Dist_list = []
 			key_list = []
 
-			for i, key in enumerate(key_dict.keys()):
-				self.response.write("%d.)%s : %s\n" % (i, key, key_dict[key]))
-
 			for i, our_dist in enumerate(L.all_district_classes):
-				"""if i > 10:
-					break"""
+
 				key_list.append(ndb.Key(db_defs.District, 'Districts'))
 				db_Dist_list.append(db_defs.District(parent=key_list[i]))
 
-
-				#our_dist = L.all_district_classes[i]
-
-				assert type(our_dist.number) is int
 				db_Dist_list[i].number = our_dist.number
+				db_Dist_list[i].state = state_key_dict[str(our_dist.state.abbr)]
+				#db_Dist_list[i].voters = []
 
+			dist_keys = ndb.put_multi(db_Dist_list)#batch put all district keys in the db, store the keys in dist_keys
 
-				assert str(our_dist.state.abbr) in key_dict
-				db_Dist_list[i].state = key_dict[str(our_dist.state.abbr)]
+			#===============PUT A COPY OF EVERY NEWLY CREATED DISTRICT KEY IN IT'S PROPER STATE LIST IN THE DB=================
+			#extracts state object from dist_key, assigne the dist_key as an element in the state's dist_key_list...just trust me
 
-				self.response.write("db_Dist_list[%d].state = %s\tkey_dict[str(our_dist.state.abbr)] = %s" % (i, db_Dist_list[i].state, key_dict[str(our_dist.state.abbr)]))
-				assert (type(db_Dist_list[i].state) == type(key_dict[str(our_dist.state.abbr)]))
+			
+			state_dist_keys_dict = {}
+			state_to_put_list = []
+			for state_key in state_keys:
+				dist_key_list = []
+				for dist_key in dist_keys:
+					if dist_key.get().state == state_key:
+						state_dist_keys_dict[state_key] = dist_key_list.append(dist_key)
 
-				db_Dist_list[i].voters = []
+				state_dist_keys_dict[state_key] = dist_key_list
+				state_to_put = state_key.get()
+				state_to_put.dist_key_list = dist_key_list
 
-				#db_Dist_list.append(db_Dist_list[i])
+				state_to_put_list.append(state_to_put)
 
-				#db_Dist_list[i].put()
-			ndb.put_multi(db_Dist_list)
-			self.response.write("db writing complete\n\n")
+				#state_to_put.put()
+				#break
+
+			ndb.put_multi(state_to_put_list)
+				
+
 
 	def delete(self):
-		self.response.write("this is a delete\n\n")
 
 		if self.request.get('nuke') == 'True':
 			self.nuke()
 
+	def get(self):
+
+		if self.request.get('test')  == 'True':
+
+			qry = db_defs.State.query()
+			states = qry.fetch()
+
+			for state in states:
+				self.response.write("%s: " % (state.abbr))
+				for i, dist in enumerate(state.dist_key_list):
+					state_got = dist.get().state.get()
+					self.response.write("%d) dist data: state = %s; number = %d\n" % (i, dist.get().state.get().abbr, dist.get().number))
+
 	def nuke(self):
-		self.response.write("nuking\n\n")
 
 		state_qry = db_defs.State.query()
 		qo = ndb.QueryOptions(keys_only=True)
@@ -93,5 +111,5 @@ class Admin(base_page.BaseHandler):
 		qo = ndb.QueryOptions(keys_only=True)
 		nuke_dists = dist_qry.fetch(433, options=qo)
 
-		#ndb.delete_multi(nuke_states)
+		ndb.delete_multi(nuke_states)
 		ndb.delete_multi(nuke_dists)
